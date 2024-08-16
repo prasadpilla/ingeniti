72484f30ed5fc0272ce7d10fbbc89a51c8b446f6
Devang Vishnu <thedevangvishnu@gmail.com>
2024-08-16 16:15:41 +0530

    Bring over the exisiting project libs, nav and structure to new setup

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { CombinedDarkTheme, CombinedDefaultTheme } from './components/theme';
+import { ThemeContext } from './context/theme-context';
+import React from 'react';
+import { PaperProvider } from 'react-native-paper';
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import HomeScreen from './screens/home';
+import ProfileScreen from './screens/profile';
+import SettingsScreen from './screens/settings';
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
+  const [isThemeDark, setIsThemeDark] = React.useState(false);
+  const theme = isThemeDark ? CombinedDarkTheme : CombinedDefaultTheme;
+
+  const toggleTheme = React.useCallback(() => {
+    return setIsThemeDark(!isThemeDark);
+  }, [isThemeDark]);
+
+  const preferences = React.useMemo(
+    () => ({
+      toggleTheme,
+      isThemeDark,
+    }),
+    [toggleTheme, isThemeDark]
+  );
+
   return (
-    <View style={styles.container}>
-      <Text style={{ color: '#eee' }}>Ingeniti Mobile App 🎉</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={preferences}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer theme={theme}>
+          <Tab.Navigator>
+            <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="home" color={color} size={26} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Profile"
+              component={ProfileScreen}
+              options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="account" color={color} size={26} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Settings"
+              component={SettingsScreen}
+              options={{
+                tabBarLabel: 'Settings',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="cog" color={color} size={26} />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </ThemeContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#222',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
