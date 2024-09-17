c3dafd1ef261b4cc64a297f0a737bf633e12c996
Devang Vishnu <thedevangvishnu@gmail.com>
2024-09-17 15:05:46 +0530

    Install i18n, configure and add translations

---
+++ apps/mobile/utils/i18next/i18next.ts
@@ -0,0 +1,21 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './locales/en.json';
+import es from './locales/es.json';
+
+export const resources = {
+  en: { translation: en },
+  es: { translation: es },
+};
+
+i18next.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18next;
