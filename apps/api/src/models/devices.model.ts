d29963f0823caddf7f0a7eeddaaf913d1e18703c
Devang Vishnu <thedevangvishnu@gmail.com>
2024-09-30 20:33:41 +0530

    Expand devices model

--- apps/api/src/models/devices.model.ts
+++ apps/api/src/models/devices.model.ts
@@ -1,14 +1,33 @@
 import { and, eq } from 'drizzle-orm';
-import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, text, timestamp, uuid, numeric, boolean } from 'drizzle-orm/pg-core';
 import { db } from '../db/client';
 
 export const devices = pgTable('devices', {
   id: uuid('id').primaryKey().defaultRandom(),
   userId: text('user_id').notNull(),
-  name: text('name'),
-  model: text('model'),
-  identifier: text('identifier'),
+  name: text('name').notNull(),
+  model: text('model').notNull(),
+  identifier: text('identifier').notNull(),
+  serialNumber: text('serial_number').notNull(),
+  usage: text('usage').notNull(),
+  type: text('type').notNull(),
+  location: text('location').notNull(),
+  averageEnergyCost: numeric('average_energy_cost').notNull(),
+  minOffTime: numeric('min_off_time').notNull(),
+  brownOutVoltageChange: numeric('brown_out_voltage_change').notNull(),
+  brownOutFrequencyChange: numeric('brown_out_frequency_change').notNull(),
+  utility: text('utility').notNull(),
+  country: text('country').notNull(),
+  meterServiceID: text('meter_service_id').notNull(),
+  isConnectedToPrimaryDevice: boolean('is_connected_to_primary_device').notNull(),
+  utilitySmartPanel: text('utility_smart_panel').notNull(),
+  countrySmartPanel: text('country_smart_panel').notNull(),
+  meterServiceIDSmartPanel: text('meter_service_id_smart_panel').notNull(),
+  maxLoad: numeric('max_load').notNull(),
   createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .defaultNow()
+    .$onUpdate(() => new Date()),
 });
 
 export function getDevice(userId: string, deviceId: string) {
