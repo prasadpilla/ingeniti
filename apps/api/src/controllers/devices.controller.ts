bd945928160ad065935c42840b0b877e5e02711a
Devang Vishnu <thedevangvishnu@gmail.com>
2024-10-01 12:20:13 +0530

    Write the device post controller

--- apps/api/src/controllers/devices.controller.ts
+++ apps/api/src/controllers/devices.controller.ts
@@ -1,6 +1,6 @@
 import { WithAuthProp } from '@clerk/clerk-sdk-node';
-import { Response } from 'express';
-import { GenericError, deviceOnBoardingFormSchema } from '@ingeniti/shared';
+import { Request, Response } from 'express';
+import { GenericError, HttpStatusCode, deviceOnBoardingFormSchema } from '@ingeniti/shared';
 import { insertDevice } from '../models/devices.model';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -11,6 +11,9 @@ devicesController.post(
   '/',
   async (req: WithAuthProp<Request>, res: Response<{ success: boolean; id: string } | GenericError>) => {
     const validatedBody = deviceOnBoardingFormSchema.parse(req.body);
+    if (!validatedBody) {
+      return res.status(HttpStatusCode.BAD_REQUEST_400).json({ success: false, error: 'Invalid request body' });
+    }
     const userId = req.auth.userId as string;
 
     const device = await insertDevice({
@@ -18,7 +21,7 @@ devicesController.post(
       ...validatedBody,
     });
 
-    res.json({ success: true, id: device.id });
+    res.status(HttpStatusCode.CREATED_201).json({ success: true, id: device.id });
   }
 );
 
